f451cb649dbc65bc75eb127a373c9e3b75ab73a7
Farzana Chowdhury <sumaiya.farzana.eleen@gmail.com>
2022-09-17 22:49:28 +0600

    Adding results

--- Course_Entry.php
+++ Course_Entry.php
@@ -1,4 +1,6 @@
 <?php
+
+use Shuchkin\SimpleXLSX;
 $server_name="localhost";
 $username="root";
 $password="";
@@ -37,7 +39,71 @@ else{
 } 
 
 }
+else if(!isset($_POST['add']))
+{
+   echo"Hello";
+}
 else
 {
     echo"Enter Course code";
 }
+
+echo'
+<br><br>
+<form action="" method ="POST" enctype="multipart/form-data">
+<input type="file" name="excel">
+<input type="submit" name="submit"> 
+</form>';
+
+
+if(isset($_FILES['excel']['name']))
+{
+    include "xlsx.php";
+
+
+
+    $excel=SimpleXLSX::parse($_FILES['excel']['tmp_name']);
+    echo'<pre>';
+    $i=0;
+    //  print_r($excel->rows());
+    foreach($excel->rows() as $key => $row )
+    {
+       
+    $q=" ";
+    foreach ($row as $key => $cell){
+    if($i==0)
+    {
+    //    $q. so that it keeps adding the contents
+        $q.=$cell." varchar(50),"; //So that all types will be varchar in Create table query
+    }
+   
+    else
+    {
+
+        $q.="'".$cell."',";
+    }
+   
+}
+    if($i==0)
+    {
+        $sql_query="CREATE TABLE ".$excel->sheetname(0)." (".rtrim($q,",").");";
+    }
+    else
+    {
+        $sql_query="INSERT INTO ".$excel->sheetname(0)." VALUES (".rtrim($q,",").");";
+    }
+    if(mysqli_query($conn,$sql_query))
+    {
+        echo "true";
+    }
+    $i++;
+    echo $sql_query; 
+    echo'<br>';
+    
+    }
+  
+ 
+
+}
+?>
+</html>
